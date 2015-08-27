61e84df579c70e047cea50c4007d449adb4ae5cd
Stanley <kill.xmurderer@gmail.com>
2015-08-27 15:33:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:ondrej/php5-5.6
 sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install apache2
-sudo add-apt-repository ppa:ondrej/php5-5.6
 sudo apt-get install php5
 sudo apt-get install mysql-server
 sudo apt-get install php5-mysql
