5294d2503a4958ced05ea992621d870a9871ebde
Stanley <kill.xmurderer@gmail.com>
2015-11-06 16:56:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@ sudo add-apt-repository ppa:ondrej/php5-5.6
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install apache2
-sudo apt-get install php5
-sudo apt-get install mysql-server
-sudo apt-get install php5-mysql
-sudo apt-get install php5-json
-sudo apt-get install unzip
-sudo apt-get install curl
-sudo apt-get install openssl
-sudo apt-get install php5-mcrypt
-sudo php5enmod mcrypt
-sudo apt-get install -y git-core
-sudo a2enmod rewrite
+sudo apt-get install apache2 -y
+sudo apt-get install php5 -y
+sudo apt-get install mysql-server -y
+sudo apt-get install php5-mysql -y
+sudo apt-get install php5-json -y
+sudo apt-get install unzip -y
+sudo apt-get install curl -y
+sudo apt-get install openssl -y
+sudo apt-get install php5-mcrypt -y
+sudo php5enmod mcrypt -y
+sudo apt-get install -y git-core -y
+sudo a2enmod rewrite -y
 sudo service apache2 restart
 sudo apt-get install mongodb
 
